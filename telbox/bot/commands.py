1767ac0b2e36fd1cc92ac1e1a7b690800b8bf0a2
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2020-12-17 20:23:09 +0330

    [fix] remove ipdb

--- telbox/bot/commands.py
+++ telbox/bot/commands.py
@@ -21,8 +21,4 @@ def caps(update: Update, context: Context):
 @command("upload")
 def upload(update: Update, context: Context):
     readme = Path("/home/mehdi/Documents/Projects/telbox/telbox/README.md")
-    import ipdb
-
-    ipdb.set_trace()
-    print(readme)
-    context.bot.send_document(chat_id=update.effective_chat.id, document=readme)
+    context.bot.send_document(chat_id=update.effective_chat.id, document=open(readme))
